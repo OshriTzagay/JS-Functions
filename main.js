e83cdcbb14cdcfb08cba9ff0b5fc5994d52c24cb
OshriTzagay <oshritzagay@gmail.com>
2021-08-09 20:35:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// var array = ['a','b','c','d','e','f','g']
+// var x = array.length
+
+// for(var i = 0; i < array.length ; i++){
+//     console.log(array[x])
+//     x=x-1
+// }
+//////////////////// ANOTHER OPTION FOR MISSION 1 /////////////////////////////////
+
+// var numberArray = [3,1,8,4];
+// function getArray (numArr=[]){
+// numArr.reverse()
+// console.log(numArr)
+// }
+// getArray(numberArray);
+
+
+
+////////////Mission2//////////////
+
+// function tzugFunction(numbers) {
+//   if (numbers.length == 10) {
+//     numbers.splice(3, 0, "-");//(index , kama, replace)//
+//     numbers.splice(7, 0, "-");
+//     numbers.join();
+//     console.log(numbers.join(""));
+//   } else {
+//     numbers.splice(2, 0, "-");
+//     numbers.splice(6, 0, "-");
+//     numbers.join();
+//     console.log(numbers.join(""));
+//   }
+// }
+// tzugFunction([0, 5, 2, 6, 3, 6, 7, 7, 2, 3]);
+// tzugFunction([0, 8, 9, 4, 2, 1, 4, 5, 2]);
+
+
+
+
+///////////////////Mssion 4 //////////
+
+// var x = 1;
+// function myFunction (Num){
+// for(var i=1;i<=Num;i++){
+
+//     x = x * i;
+// }
+// return x;
+// }
+// console.log(myFunction(7));
+
+
+/////////////////////Mission 5.1/////////////////
+// var sum = 0;
+// function tzugFunction(num1,num2){
+// var sum = num1 + num2;
+
+// console.log(sum);
+
+// }
+// tzugFunction(20,25);
+/////////////////////Mission 5.2/////////////////
+// var sum = 0;
+// function tzugFunction(num1,num2){
+// var sum = num1 - num2;
+
+// console.log(sum);
+
+// }
+// tzugFunction(70,25);
+/////////////////////Mission 5.3/////////////////
+// var kefel = 0;
+// function tzugFunction(num1,num2){
+// var kefel = num1 * num2;
+
+// console.log(kefel);
+
+// }
+// tzugFunction(70,25);
+///////////////////Mission 5.4/////////////////
+// var hiluk = 0;
+// function tzugFunction(num1,num2){
+// var hiluk = num1 / num2;
+
+// console.log(hiluk);
+
+// }
+// tzugFunction(70,25);
+
+
+var theArray = [23,41,12]
+
+
